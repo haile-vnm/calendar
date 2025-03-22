da05eff0e10d69244861656cffb071965fa65f06
Hai L <haile.vnm@gmail.com>
2025-03-22 23:14:38 +0700

    feat(appointment): prevent create the past appointment

--- src/app/shared/components/edit-appointment/edit-appointment.component.ts
+++ src/app/shared/components/edit-appointment/edit-appointment.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input, OnInit, output } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, output, signal } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { Appointment } from '../../../models/appointment';
 import { MatInputModule } from '@angular/material/input';
@@ -10,8 +10,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AppointmentService } from '../../services/appointment.service';
 import { CommonModule } from '@angular/common';
-import { greaterThan } from '../../../validators/greater-datetime';
-import { ReplaySubject } from 'rxjs';
+import { distinctUntilChanged, map, ReplaySubject } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { createDateFrom } from '../../../utils/date';
 
 @Component({
   selector: 'app-edit-appointment',
@@ -33,16 +34,17 @@ export class EditAppointmentComponent implements OnInit {
   private fb = inject(FormBuilder);
   private data = inject<Partial<Appointment>>(MAT_DIALOG_DATA);
   private appointmentService = inject(AppointmentService);
+  private destroyRef = inject(DestroyRef);
 
   appointment = input<Partial<Appointment>>();
+  appointmentId = computed(() => this.appointment()?.id || this.data.id);
+  errorMessage = signal('');
 
   complete = output();
-  appointmentId = computed(() => this.appointment()?.id || this.data.id)
 
   form!: FormGroup;
 
   ngOnInit(): void {
-    const form$ = new ReplaySubject<FormGroup>(1);
     this.form = this.fb.group({
       title: [this.appointment()?.title || this.data.title || '', Validators.required],
       startDate: [this.appointment()?.start || this.data.start || '', Validators.required],
@@ -50,13 +52,18 @@ export class EditAppointmentComponent implements OnInit {
       startTime: [this.appointment()?.start || this.data.start || '', Validators.required],
       endTime: this.fb.control(
         this.appointment()?.end || this.data.end || '',
-        { validators: Validators.required, asyncValidators: greaterThan('startDate', 'startTime', 'endDate', form$) }
+        Validators.required
       ),
     });
-    form$.next(this.form);
+
+    this.validateEventTime();
   }
 
   submitForm() {
+    if (this.errorMessage()) {
+      return;
+    }
+
     const id = this.appointmentId();
     if (id) {
       this.appointmentService.update(id, this.getAppointmentChanges());
@@ -89,5 +96,24 @@ export class EditAppointmentComponent implements OnInit {
         this.form.value.endTime.getMinutes()
       ),
     };
+  }
+
+  private validateEventTime() {
+    this.form.valueChanges.pipe(
+      map(({ startDate, startTime, endDate, endTime }) => {
+        if (!(startDate && startTime && endDate && endTime)) {
+          return true;
+        }
+
+        const start = createDateFrom(startDate, startTime);
+        const end = createDateFrom(endDate, endTime);
+
+        return start < end && start > new Date();
+      }),
+      distinctUntilChanged(),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(ok => {
+      this.errorMessage.set(ok ? '' : 'Only allow to create future event.');
+    });
   }
 }
